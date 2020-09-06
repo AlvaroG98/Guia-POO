6d163973300ecb81dbba1034f5c0dc49a2791a77
AlvaroG98 <201700377@ujmd.edu.sv>
2020-09-06 15:54:32 -0600

    primer commit

---
+++ Ejercicio3/src/ejercicio3/main.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio3;
+
+/**
+ *
+ * @author GUILLERMO
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+     public static void main(String[] args) {
+
+        Perro perro = new Perro("Bruno","Purina",10,"Shiba-inu");
+        Gato gato = new Gato("Pascal","Wishkas",7,"Azules");
+        Gallo gallo = new Gallo("Toto","Semillas",2,"Verde");
+        Hamster hamster = new Hamster("Hamtaro","lechuga",5,10);
+
+        //-->Nos muestra los detalles del objeto
+        perro.mostrar();
+        System.out.println("--------------------------------------------------");
+        gato.mostrar();
+        System.out.println("--------------------------------------------------");
+        gallo.mostrar();
+        System.out.println("--------------------------------------------------");
+        hamster.mostrar();
+
+    }
+    
+}
